df220f986a941f29b979da2e37db779e549e7860
soledad921 <31821448+soledad921@users.noreply.github.com>
2020-06-27 04:44:23 +0200

    Update models.py

--- tkbc/models.py
+++ tkbc/models.py
@@ -609,7 +609,7 @@ class TGeomE2(TKBCModel):
 	
         if self.pre_train:
             self.embeddings[0].weight.data[:,self.rank:self.rank*3] *= 0
-            self.embeddings[1].weight.data[:,self.rank:self.rank*3] *= 0
+      #      self.embeddings[1].weight.data[:,self.rank:self.rank*3] *= 0
       #      self.embeddings[2].weight.data[:,self.rank:self.rank*3] *= 0
         
 
